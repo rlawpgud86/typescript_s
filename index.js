45395289c09e716a962301be2a09b2313465ba9e
jehyung2 <rlawpgud86@naver.com>
2021-01-08 12:25:29 +0900

    타입스크립트 기본 셋팅 완료

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Kimmad", age = 36, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, and you are a ${gender}`);
+};
+sayHi(name, age);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,10 @@
+const name = "Kimmad",
+age = 36,
+gender = "male";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, and you are a ${gender}`)
+}
+
+sayHi(name, age)
+export {};
